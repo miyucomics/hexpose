ff38a0e1ea2ba0f27642055f96c840b3ce73d801
miyucomics <nbriannyc@gmail.com>
2025-12-21 19:33:50 -0500

    Added iotatype bijection

--- src/main/java/miyucomics/hexpose/interop/MoreiotasInteropActions.kt
+++ src/main/java/miyucomics/hexpose/interop/MoreiotasInteropActions.kt
@@ -1,12 +1,15 @@
 package miyucomics.hexpose.interop
 
 import at.petrak.hexcasting.api.casting.math.HexDir
+import at.petrak.hexcasting.common.lib.HexRegistries
+import at.petrak.hexcasting.xplat.IXplatAbstractions
 import miyucomics.hexpose.HexposeActions.register
 import miyucomics.hexpose.actions.interop.OpIotaBijection
 import miyucomics.hexpose.iotas.identifier.IdentifierIota
 import miyucomics.hexpose.iotas.item_stack.ItemStackIota
 import net.minecraft.registry.Registries
 import ram.talia.moreiotas.api.casting.iota.EntityTypeIota
+import ram.talia.moreiotas.api.casting.iota.IotaTypeIota
 import ram.talia.moreiotas.api.casting.iota.ItemTypeIota
 
 object MoreiotasInteropActions {
@@ -14,6 +17,11 @@ object MoreiotasInteropActions {
 		register("moreiotas_entitytype_compat", "adwaq", HexDir.EAST, OpIotaBijection<IdentifierIota, EntityTypeIota>("entitytype", IdentifierIota.TYPE, EntityTypeIota.TYPE,
 			{ IdentifierIota(Registries.ENTITY_TYPE.getId(it.entityType)) },
 			{ if (Registries.ENTITY_TYPE.containsId(it.identifier)) EntityTypeIota(Registries.ENTITY_TYPE.get(it.identifier)) else null }
+		))
+
+		register("moreiotas_iotatype_compat", "adwawd", HexDir.EAST, OpIotaBijection<IdentifierIota, IotaTypeIota>("iotatype", IdentifierIota.TYPE, IotaTypeIota.TYPE,
+			{ IdentifierIota(IXplatAbstractions.INSTANCE.iotaTypeRegistry.getId(it.iotaType)!!) },
+			{ if (IXplatAbstractions.INSTANCE.iotaTypeRegistry.containsId(it.identifier)) IotaTypeIota(IXplatAbstractions.INSTANCE.iotaTypeRegistry.get(it.identifier)!!) else null }
 		))
 
 		register("moreiotas_itemtype_compat", "adwaqa", HexDir.EAST, OpIotaBijection<IdentifierIota, ItemTypeIota>("itemtype", IdentifierIota.TYPE, ItemTypeIota.TYPE,
