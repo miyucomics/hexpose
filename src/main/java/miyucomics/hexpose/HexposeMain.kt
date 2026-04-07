7343c465d1fcf84fa47df39a407c653ac242990d
miyucomics <72044529+miyucomics@users.noreply.github.com>
2026-04-07 16:42:20 -0400

    Register the new iotas

--- src/main/java/miyucomics/hexpose/HexposeMain.kt
+++ src/main/java/miyucomics/hexpose/HexposeMain.kt
@@ -2,6 +2,8 @@ package miyucomics.hexpose
 
 import at.petrak.hexcasting.common.lib.hex.HexIotaTypes
 import miyucomics.hexpose.iotas.DisplayIota
+import miyucomics.hexpose.iotas.EnchantmentIota
+import miyucomics.hexpose.iotas.StatusEffectIota
 import miyucomics.hexpose.utils.ChatHandler
 import net.fabricmc.api.ModInitializer
 import net.minecraft.registry.Registry
@@ -10,6 +12,9 @@ import net.minecraft.util.Identifier
 class HexposeMain : ModInitializer {
 	override fun onInitialize() {
 		Registry.register(HexIotaTypes.REGISTRY, id("display"), DisplayIota.TYPE)
+		Registry.register(HexIotaTypes.REGISTRY, id("enchantment"), EnchantmentIota.TYPE)
+		Registry.register(HexIotaTypes.REGISTRY, id("status_effect"), StatusEffectIota.TYPE)
+
 		ChatHandler.init()
 		HexposeActions.init()
 	}
